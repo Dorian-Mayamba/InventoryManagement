34c11d5e0a5aad573a9df0db288b657b2d2ed6c4
Dorian Mayamba <dorian.mayamba@gmail.com>
2024-09-09 01:45:42 +0100

    edited product repo

--- data/repositories/ProductRepository.cs
+++ data/repositories/ProductRepository.cs
@@ -8,29 +8,6 @@ namespace InventoryManagement.Data.Repositories
         {
             _context = context;
         }
-        public async Task AddAsync(Product e)
-        {
-            _context.products.Add(e);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var product = await _context.products.FindAsync(id);
-            if(product == null)
-            {
-                throw new Exception($"Product with id {id} not found");
-            }
-            _context.products.Remove(product);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task EditAsync(Product e)
-        {
-            _context.Entry(e).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-        }
-
         public async Task<IEnumerable<Product>> GetAsync()
         {
             return await _context.products.
@@ -56,6 +33,28 @@ namespace InventoryManagement.Data.Repositories
                 throw new Exception($"Product with id {id} not found");
             }
             return product;
+        }
+        public async Task AddAsync(Product e)
+        {
+            _context.products.Add(e);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var product = await _context.products.FindAsync(id);
+            if(product == null)
+            {
+                throw new Exception($"Product with id {id} not found");
+            }
+            _context.products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditAsync(Product e)
+        {
+            _context.Entry(e).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
         }
     }
 }
